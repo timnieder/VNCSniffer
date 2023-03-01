145c415c63f963add096b24cb51d3525623a634c
Tim <149380950+timnieder@users.noreply.github.com>
2023-03-01 15:20:47 +0100

    parse func (>mfw no framebuffer)

--- VNCSniffer/VNCSniffer.Cli/Messages/Messages.cs
+++ VNCSniffer/VNCSniffer.Cli/Messages/Messages.cs
@@ -349,11 +349,26 @@ namespace VNCSniffer.Cli.Messages
             public ushort w;
             public ushort h;
             public int encoding;
-            public int dataLength; //TODO: make this into data
 
             public override string ToString()
             {
-                return $"Rectangle: X ({x}), Y ({y}), W ({w}), H ({h}), Encoding ({encoding}), Length ({dataLength})";
+                return $"Rectangle: X ({x}), Y ({y}), W ({w}), H ({h}), Encoding ({encoding})";
+            }
+        }
+
+        public class FramebufferUpdateEvent
+        {
+            public ushort x;
+            public ushort y;
+            public ushort w;
+            public ushort h;
+
+            public FramebufferUpdateEvent(ushort x, ushort y, ushort w, ushort h)
+            {
+                this.x = x;
+                this.y = y;
+                this.w = w;
+                this.h = h;
             }
         }
         public static ProcessStatus HandleServerFramebufferUpdate(MessageEvent ev)
@@ -374,6 +389,7 @@ namespace VNCSniffer.Cli.Messages
 
             var index = 4;
             var rectangles = new List<Rectangle>();
+            //TODO: save the current state (i and buffer index) if we need more data but have parsed smth already, so we dont double parse
             for (var i = 0; i < numberOfRectangles; i++)
             {
                 if (ev.Data.Length <= index + 12) // header check
@@ -385,21 +401,21 @@ namespace VNCSniffer.Cli.Messages
                 var h = BinaryPrimitives.ReadUInt16BigEndian(ev.Data[(index + 6)..]);
                 var encoding = BinaryPrimitives.ReadInt32BigEndian(ev.Data[(index + 8)..]);
                 index += 12;
-                //TODO: we also need to parse the data or at least skip it...
-                var dataLength = 0;
                 if (Encodings.Encodings.Handlers.TryGetValue(encoding, out var enc))
                 {
-                    dataLength = enc.GetLength(x, y, w, h, ev.Connection.Format);
+                    var e = new FramebufferUpdateEvent(x, y, w, h);
+                    var status = enc.Parse(ev, e, ref index);
+                    if (status == ProcessStatus.NeedsMoreBytes)
+                        return ProcessStatus.NeedsMoreBytes;
+
+                    // else it was handled
                 }
                 else
                 {
                     Console.WriteLine($"Encoding {encoding} not supported"); //TODO: can we do anything else?
                     //TODO: break? fallthrough and assume length is 0?
                 }
-                rectangles.Add(new Rectangle() { x = x, y = y, w = w, h = h, encoding = encoding, dataLength = dataLength });
-                index += dataLength;
-                if (ev.Data.Length < index) // data check
-                    return ProcessStatus.NeedsMoreBytes;
+                rectangles.Add(new Rectangle() { x = x, y = y, w = w, h = h, encoding = encoding });
             }
             ev.Log($"FramebufferUpdate: Rectangles ({numberOfRectangles}): {string.Join(";", rectangles)}");
             return ProcessStatus.Handled;
