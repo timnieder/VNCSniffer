c29053518827b3ab1c9b6a3b01bafec4596990a5
Tim <149380950+timnieder@users.noreply.github.com>
2023-03-07 15:56:04 +0100

    ZRLE (please test your code soon)

--- VNCSniffer/VNCSniffer.Cli/Encodings/TRLEEncoding.cs
+++ VNCSniffer/VNCSniffer.Cli/Encodings/TRLEEncoding.cs
@@ -27,42 +27,47 @@ namespace VNCSniffer.Cli.Encodings
         }
         public ProcessStatus Parse(MessageEvent e, FramebufferUpdateEvent ev, ref int index)
         {
-            var bpp = e.Connection.Format != null ? e.Connection.Format.BitsPerPixel : 32;
+            return Decode(e.Data, e.Connection.Format, ev, ref index, 16);
+        }
+
+        public static ProcessStatus Decode(ReadOnlySpan<byte> data, PixelFormat? format, FramebufferUpdateEvent ev, ref int index, byte tileSize)
+        {
+            var bpp = format != null ? format.BitsPerPixel : 32;
             bpp /= 8;
             // check if cpixels are smaller
-            if (e.Connection.Format != null &&
-                e.Connection.Format.TrueColor &&
-                e.Connection.Format.BitsPerPixel == 32)
+            if (format != null &&
+                format.TrueColor &&
+                format.BitsPerPixel == 32)
             {
                 // Check if we can fit all color bits into 3 bytes
-                var maxValue = e.Connection.Format.RedMax << e.Connection.Format.RedShift |
-                    e.Connection.Format.GreenMax << e.Connection.Format.GreenShift |
-                    e.Connection.Format.BlueMax << e.Connection.Format.BlueShift;
-                if (e.Connection.Format.BigEndian) //TODO: check if this is right?
+                var maxValue = format.RedMax << format.RedShift |
+                    format.GreenMax << format.GreenShift |
+                    format.BlueMax << format.BlueShift;
+                if (format.BigEndian) //TODO: check if this is right?
                     maxValue = BinaryPrimitives.ReverseEndianness(maxValue);
-                if ((maxValue & 0xFF000000) == 0) // only 3 bytes used
+                if ((maxValue & 0xFF000000) == 0) // higher bits empty?
                     bpp = 3;
             }
             // rectangle is tiled into 16px*16px, so math.ceil(w/16) tiles per row and math.ceil(h/16) tiles per column
-            var numTilesColumn = (int)Math.Ceiling(ev.w / 16f);
-            var numTilesRow = (int)Math.Ceiling(ev.h / 16f);
+            var numTilesColumn = (int)Math.Ceiling(ev.w / (float)tileSize);
+            var numTilesRow = (int)Math.Ceiling(ev.h / (float)tileSize);
             var numTiles = numTilesColumn * numTilesRow;
             var tileX = ev.x;
             var tileY = ev.y;
-            for (var i = 0; i < numTilesRow; i++, tileY += 16)
+            for (var i = 0; i < numTilesRow; i++, tileY += tileSize)
             {
                 // the last row can be smaller than 16px high
-                var tileH = i == numTilesRow - 1 ? ev.h % 16 : 16;
-                for (var j = 0; j < numTilesColumn; j++, tileX += 16)
+                var tileH = i == numTilesRow - 1 ? ev.h % tileSize : tileSize;
+                for (var j = 0; j < numTilesColumn; j++, tileX += tileSize)
                 {
                     // may not have enough bytes for the header
-                    if (e.Data.Length < index)
+                    if (data.Length < index)
                         return ProcessStatus.NeedsMoreBytes;
 
                     // last tile in a row can be smaller than 16px wide
-                    var tileW = j == numTilesColumn - 1 ? ev.w % 16 : 16;
+                    var tileW = j == numTilesColumn - 1 ? ev.w % tileSize : tileSize;
 
-                    var subencoding = e.Data[index];
+                    var subencoding = data[index];
                     index += 1;
                     //TODO: make into pixel array
                     ReadOnlySpan<byte> palette = null;
@@ -72,7 +77,7 @@ namespace VNCSniffer.Cli.Encodings
                         case SubencodingType.Raw: //TODO: just call RawEncoding.Handle here?
                             {
                                 var length = tileW * tileH * bpp;
-                                if (e.Data.Length < index + length)
+                                if (data.Length < index + length)
                                     return ProcessStatus.NeedsMoreBytes;
 
                                 //TODO: parse bitmap
@@ -81,11 +86,11 @@ namespace VNCSniffer.Cli.Encodings
                             }
                         case SubencodingType.SolidTile: // Solid color tile
                             {
-                                if (e.Data.Length < index + bpp)
+                                if (data.Length < index + bpp)
                                     return ProcessStatus.NeedsMoreBytes;
 
                                 //TODO: parse color
-                                var clr = e.Data[index];
+                                var clr = data[index];
                                 index += bpp;
                                 break;
                             }
@@ -95,12 +100,12 @@ namespace VNCSniffer.Cli.Encodings
                                 var paletteSize = (byte)t;
                                 var paletteSizeInBytes = paletteSize * bpp;
                                 var packedPixelsLength = GetPacketPixelsSize(paletteSize, tileW, tileH);
-                                if (e.Data.Length < index + paletteSizeInBytes + packedPixelsLength)
+                                if (data.Length < index + paletteSizeInBytes + packedPixelsLength)
                                     return ProcessStatus.NeedsMoreBytes;
 
-                                palette = e.Data[(index)..(index + paletteSizeInBytes)];
+                                palette = data[(index)..(index + paletteSizeInBytes)];
                                 index += paletteSizeInBytes;
-                                HandlePackedPixels(e, ref index, tileH, tileW, palette, paletteSize, packedPixelsLength);
+                                HandlePackedPixels(data, ref index, tileH, tileW, palette, paletteSize, packedPixelsLength);
                                 break;
                             }
                         case SubencodingType.ReusePalette:
@@ -108,10 +113,10 @@ namespace VNCSniffer.Cli.Encodings
                                 // read using palette
                                 var paletteSize = (byte)(palette.Length / bpp);
                                 var packedPixelsLength = GetPacketPixelsSize(paletteSize, tileW, tileH);
-                                if (e.Data.Length < index + packedPixelsLength)
+                                if (data.Length < index + packedPixelsLength)
                                     return ProcessStatus.NeedsMoreBytes;
 
-                                HandlePackedPixels(e, ref index, tileH, tileW, palette, paletteSize, packedPixelsLength);
+                                HandlePackedPixels(data, ref index, tileH, tileW, palette, paletteSize, packedPixelsLength);
                                 break;
                             }
                         case SubencodingType.PlainRLE:
@@ -120,20 +125,20 @@ namespace VNCSniffer.Cli.Encodings
                                 var tilePixels = tileH * tileW;
                                 while (tilePixels > 0)
                                 {
-                                    if (e.Data.Length < index + bpp + 1) // atleast one 
+                                    if (data.Length < index + bpp + 1) // atleast one 
                                         return ProcessStatus.NeedsMoreBytes;
-                                    
-                                    var pixelValue = e.Data[(index)..(index + bpp)];
+
+                                    var pixelValue = data[(index)..(index + bpp)];
                                     index += bpp;
                                     var length = 0;
-                                    while (e.Data[index] == 255)
+                                    while (data[index] == 255)
                                     {
-                                        length += e.Data[index];
-                                        if (e.Data.Length < index + 1) // length not yet done, so we still need data
+                                        length += data[index];
+                                        if (data.Length < index + 1) // length not yet done, so we still need data
                                             return ProcessStatus.NeedsMoreBytes;
                                         index++;
                                     }
-                                    length += e.Data[index];
+                                    length += data[index];
                                     index++;
                                     //TODO: draw the run
                                     tilePixels -= length;
@@ -143,7 +148,7 @@ namespace VNCSniffer.Cli.Encodings
                         case SubencodingType.ReusePaletteRLE:
                             {
                                 // read RLE using palette
-                                var handled = HandlePaletteRLE(e, ref index, tileH, tileW, palette);
+                                var handled = HandlePaletteRLE(data, ref index, tileH, tileW, palette);
                                 if (handled == ProcessStatus.NeedsMoreBytes)
                                     return handled;
                                 break;
@@ -153,12 +158,12 @@ namespace VNCSniffer.Cli.Encodings
                                 // read palette
                                 var paletteSize = (byte)t - 128;
                                 var paletteSizeInBytes = paletteSize * bpp;
-                                if (e.Data.Length < index + paletteSizeInBytes)
+                                if (data.Length < index + paletteSizeInBytes)
                                     return ProcessStatus.NeedsMoreBytes;
 
-                                palette = e.Data[index..(index + paletteSizeInBytes)];
+                                palette = data[index..(index + paletteSizeInBytes)];
                                 // do paletteRLE
-                                var handled = HandlePaletteRLE(e, ref index, tileH, tileW, palette);
+                                var handled = HandlePaletteRLE(data, ref index, tileH, tileW, palette);
                                 if (handled == ProcessStatus.NeedsMoreBytes)
                                     return handled;
                                 break;
@@ -173,32 +178,32 @@ namespace VNCSniffer.Cli.Encodings
             return ProcessStatus.Handled;
         }
 
-        private static ProcessStatus HandlePaletteRLE(MessageEvent e, ref int index, int tileH, int tileW, ReadOnlySpan<byte> palette)
+        private static ProcessStatus HandlePaletteRLE(ReadOnlySpan<byte> data, ref int index, int tileH, int tileW, ReadOnlySpan<byte> palette)
         {
             var tilePixels = tileH * tileW;
             while (tilePixels > 0)
             {
-                if (e.Data.Length < index + 1) // atleast one index
+                if (data.Length < index + 1) // atleast one index
                     return ProcessStatus.NeedsMoreBytes;
 
-                var paletteIndex = e.Data[index];
+                var paletteIndex = data[index];
                 var runLength = 1;
                 if (paletteIndex > 128) // top bit set, run longer than 1
                 {
                     paletteIndex -= 128;
                     // parse the run length
-                    if (e.Data.Length < index + 1)
+                    if (data.Length < index + 1)
                         return ProcessStatus.NeedsMoreBytes;
                     index++;
 
-                    while (e.Data[index] == 255)
+                    while (data[index] == 255)
                     {
-                        runLength += e.Data[index];
-                        if (e.Data.Length < index + 1) // length not yet done, so we still need data
+                        runLength += data[index];
+                        if (data.Length < index + 1) // length not yet done, so we still need data
                             return ProcessStatus.NeedsMoreBytes;
                         index++;
                     }
-                    runLength += e.Data[index];
+                    runLength += data[index];
                 }
                 index++;
                 var clr = palette[paletteIndex];
@@ -209,9 +214,9 @@ namespace VNCSniffer.Cli.Encodings
         }
 
         // Parses packed pixels. Expects the data to be there
-        private static void HandlePackedPixels(MessageEvent e, ref int index, int tileH, int tileW, ReadOnlySpan<byte> palette, byte paletteSize, int packedPixelsLength)
+        private static void HandlePackedPixels(ReadOnlySpan<byte> data, ref int index, int tileH, int tileW, ReadOnlySpan<byte> palette, byte paletteSize, int packedPixelsLength)
         {
-            var packedPixelsBytes = e.Data[(index)..(index + packedPixelsLength)];
+            var packedPixelsBytes = data[(index)..(index + packedPixelsLength)];
             var packedPixels = new BitArray(packedPixelsBytes.ToArray());
             //TODO: how 2 handle bits
             var indexSize = GetPaletteIndexSize(paletteSize);
@@ -225,7 +230,7 @@ namespace VNCSniffer.Cli.Encodings
                 var bitCount = (tileW * indexSize);
                 if (bitCount % 8 != 0) // skip padding if row isn't a multiple of 8
                 {
-
+                    //TODO: skip to next byte
                 }
             }
             index += packedPixelsLength;
---
+++ VNCSniffer/VNCSniffer.Cli/Encodings/ZRLEEncoding.cs
@@ -0,0 +1,30 @@
+﻿using static VNCSniffer.Cli.Messages.Messages;
+using VNCSniffer.Cli.Messages.Server;
+using System.Buffers.Binary;
+using System.IO.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression;
+using Ionic.Zlib;
+
+namespace VNCSniffer.Cli.Encodings
+{
+    public class ZRLEEncoding : IEncoding
+    {
+        public ProcessStatus Parse(MessageEvent e, FramebufferUpdateEvent ev, ref int index)
+        {
+            var length = 4;
+            if (e.Data.Length < index + length)
+                return ProcessStatus.NeedsMoreBytes;
+
+            var dataLength = BinaryPrimitives.ReadInt32BigEndian(e.Data[index..]);
+            index += length;
+            if (e.Data.Length < index + dataLength)
+                return ProcessStatus.NeedsMoreBytes;
+
+            var zlibData = e.Data[index..(index + dataLength)];
+            //TODO: can we just do this? dont we need a zlib object?
+            var data = ZlibStream.UncompressBuffer(zlibData.ToArray());
+
+            return TRLEEncoding.Decode(data, e.Connection.Format, ev, ref index, 64);
+        }
+    }
+}
